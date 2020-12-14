931a2f0b2bcfb2bf4f122580863dcaf923d67740
Mortpo <anquetin.r@gmail.com>
2020-12-14 12:04:30 +0100

    Ajout de tous les tests demandés

--- src/test/java/machine/MachineAppTest.java
+++ src/test/java/machine/MachineAppTest.java
@@ -7,9 +7,29 @@ import org.junit.Test;
 public class MachineAppTest {
 
 	@Test
-	public void test() {
-		MachineApp.main(new String[0]);
-		assertTrue(true);
+	/**
+	 * Test de runMachine 
+	 */
+	public void testrunMachineTrue() {
+		String expected = "The input was accepted.";
+		String result = "";
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "010000110101#010000110101";
+		result = MachineApp.runMachine(turingMachine1, donnee,false);
+		assertEquals(expected, result);
+	}
+
+	@Test
+	/**
+	 * Test de runMachine 
+	 */
+	public void testrunMachineFalse() {
+		String expected = "The input was rejected.";
+		String result = "";
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "d#010000110101";
+		result = MachineApp.runMachine(turingMachine1, donnee,false);
+		assertEquals(expected, result);
 	}
 
 }
---
+++ src/test/java/machine/TuringMachineTest.java
@@ -0,0 +1,97 @@
+package machine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TuringMachineTest {
+    
+    @Test
+    public void testrunTrue() {
+		Boolean expected = true;
+		Boolean result;
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "010000110101#010000110101";
+		result = turingMachine1.run( donnee, false);
+		assertEquals(expected, result);
+    }
+    
+
+    @Test
+    public void testrunFalse() {
+		Boolean expected = false;
+		Boolean result;
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "d#010000110101";
+		result = turingMachine1.run( donnee, false);
+		assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testsetAcceptStateTrue() {
+		Boolean expected = true;
+		Boolean result;
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "qa";
+		result = turingMachine1.setAcceptState( donnee);
+		assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testsetAcceptStateFalse() {
+		Boolean expected = false;
+		Boolean result;
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "qqsfqsb";
+		result = turingMachine1.setAcceptState( donnee);
+		assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testsetRejectStateTrue() {
+		Boolean expected = true;
+		Boolean result;
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "qr";
+		result = turingMachine1.setRejectState( donnee);
+		assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testsetRejectStateFalse() {
+		Boolean expected = false;
+		Boolean result;
+		TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+		String donnee = "qqsfqsb";
+		result = turingMachine1.setRejectState( donnee);
+		assertEquals(expected, result);
+    }
+
+    @Test
+    public void testaddtransitionConflictFalse(){
+        Boolean expected = false;
+        Boolean result;
+        TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords();
+        result=turingMachine1.addTransition("q1", '1', "q3", 'x', true);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testaddtransitionFalse(){
+        Boolean expected = false;
+        Boolean result;
+        TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords(); //marche pas
+        result=turingMachine1.addTransition("q1", '#', "q3", '#', true);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testaddtransitionTrue(){
+        Boolean expected = true;
+        Boolean result;
+        TuringMachine turingMachine1 = MachinesLibrary.buildEqualBinaryWords(); //marche pas
+        result=turingMachine1.addTransition("qa", '1', "q1", 'x', true);
+        assertEquals(expected, result);
+    }
+
+}
