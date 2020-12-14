c170e7cd6486947faac24758f64faafe036c1788
TebaiOsama <56879757+TebaiOsama@users.noreply.github.com>
2020-12-14 11:30:35 +0100

    Update TuringMachine.java
    Amelioration des commentaires

--- src/main/java/machine/TuringMachine.java
+++ src/main/java/machine/TuringMachine.java
@@ -23,7 +23,8 @@ public class TuringMachine {
 		char writeSymbol;
 		boolean moveDirection; // true is right, false is left
 
-		boolean isConflicting(String state, char symbol) { //fonction qui permet de savoir si l'etat et le symbol en parametres sont egaux à l'etat et au symbol actuel
+		/** fonction qui permet de savoir si l'etat et le symbol en parametres sont egaux à l'etat et au symbol actuel */
+		boolean isConflicting(String state, char symbol) { 
 			if (state.equals(readState) && symbol == readSymbol) {
 				return true;
 			} else {
@@ -32,7 +33,8 @@ public class TuringMachine {
 		}
 	}
 
-	public TuringMachine() { //Constructeur
+	/** Constructeur */
+	public TuringMachine() { 
 		stateSpace = new HashSet<String>();
 		transitionSpace = new HashSet<Transition>();
 		startState = new String("");
@@ -43,11 +45,12 @@ public class TuringMachine {
 		currentSymbol = 0;
 	}
 
-	public boolean run(String input, boolean silentmode) { //fonction de lancement de la machine
+	/** fonction de lancement de la machine */
+	public boolean run(String input, boolean silentmode) { 
 		currentState = startState;
 		tape = input;
-
-		while (!currentState.equals(acceptState) && !currentState.equals(rejectState)) { //Si l'etat actuel n'est ni le dernier ni l'etat interdit
+		/** Si l'etat actuel n'est ni le dernier ni l'etat interdit */
+		while (!currentState.equals(acceptState) && !currentState.equals(rejectState)) { 
 			boolean foundTransition = false;
 			Transition CurrentTransition = null;
 
@@ -103,8 +106,8 @@ public class TuringMachine {
 		}
 
 	}
-
-	public boolean addState(String newState) { //fonction qui ajoute l'etat "newState" dans stateSpace, mais seulement si cet etat n'est pas dejà present, sinon il return false
+	/** fonction qui ajoute l'etat "newState" dans stateSpace, mais seulement si cet etat n'est pas dejà present, sinon il return false */
+	public boolean addState(String newState) { 
 		if (stateSpace.contains(newState)) {
 			return false;
 		} else {
@@ -112,8 +115,8 @@ public class TuringMachine {
 			return true;
 		}
 	}
-
-	public boolean setStartState(String newStartState) { //fonction qui permet de definir l'etat de debut, si il est present dans stateSpace, sinon il return false
+	/** fonction qui permet de definir l'etat de debut, si il est present dans stateSpace, sinon il return false */
+	public boolean setStartState(String newStartState) { 
 		if (stateSpace.contains(newStartState)) {
 			startState = newStartState;
 			return true;
@@ -122,7 +125,8 @@ public class TuringMachine {
 		}
 	}
 
-	public boolean setAcceptState(String newAcceptState) { //fonction qui permet de definir l'etat de fin, si il est present dans stateSpace et si il est different de l'etat interdit
+	/** fonction qui permet de definir l'etat de fin, si il est present dans stateSpace et si il est different de l'etat interdit */
+	public boolean setAcceptState(String newAcceptState) { 
 		if (stateSpace.contains(newAcceptState) && !rejectState.equals(newAcceptState)) {
 			acceptState = newAcceptState;
 			return true;
@@ -131,8 +135,8 @@ public class TuringMachine {
 		}
 
 	}
-
-	public boolean setRejectState(String newRejectState) { //permet de definir l'etat interdit
+	/** permet de definir l'etat interdit */
+	public boolean setRejectState(String newRejectState) { 
 		if (stateSpace.contains(newRejectState) && !acceptState.equals(newRejectState)) {
 			rejectState = newRejectState;
 			return true;
@@ -140,15 +144,17 @@ public class TuringMachine {
 			return false;
 		}
 	}
-
-	public boolean addTransition(String rState, char rSymbol, String wState, char wSymbol, boolean mDirection) { //ajout d'une transition dans le hashSet "transitionSpace"
-		if (!stateSpace.contains(rState) || !stateSpace.contains(wState)) { //Si la liste des etats ne continent pas d'etat de lecture ni d'ecriture, cette transition ne peut pas etre ajoutée
+	/** ajout d'une transition dans le hashSet "transitionSpace" */
+	public boolean addTransition(String rState, char rSymbol, String wState, char wSymbol, boolean mDirection) { 
+		/**  Si la liste des etats ne continent pas d'etat de lecture ni d'ecriture, cette transition ne peut pas etre ajoutée */
+		if (!stateSpace.contains(rState) || !stateSpace.contains(wState)) { 
 			return false;
 		}
 
 		boolean conflict = false;
 		Iterator<Transition> TransitionsIterator = transitionSpace.iterator();
-		while (TransitionsIterator.hasNext() && conflict == false) { //Si l'iterateur possede encore des elements aprés, et si l'element d'apres ne crée pas de conflits
+		/** Si l'iterateur possede encore des elements aprés, et si l'element d'apres ne crée pas de conflits */
+		while (TransitionsIterator.hasNext() && conflict == false) { 
 			Transition nextTransition = TransitionsIterator.next();
 			if (nextTransition.isConflicting(rState, rSymbol)) { 
 				conflict = true;
@@ -157,7 +163,7 @@ public class TuringMachine {
 		}
 		if (conflict == true) {
 			return false;
-		} else { //On ajoute la transition
+		} else { /** On ajoute la transition */
 			Transition newTransition = new Transition();
 			newTransition.readState = rState;
 			newTransition.readSymbol = rSymbol;
