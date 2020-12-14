e50fdfc6b2e5dc12ff5cdea89c91f0767fafa8a5
WyderSevrin <sevrin.wyder@gmail.com>
2020-12-14 12:15:15 +0100

    Ajout de fonction test pour la classe MachineApp et TuringMachine

--- src/test/java/machine/MachineAppTest.java
+++ src/test/java/machine/MachineAppTest.java
@@ -5,11 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MachineAppTest {
-
+        TuringMachine turing = MachinesLibrary.buildEqualBinaryWords();
+        String prog="010000110101#010000110101";
+        boolean sillentMode =false;
+        
 	@Test
-	public void test() {
+	public void MahineApp_main_true() {
 		MachineApp.main(new String[0]);
 		assertTrue(true);
+	}
+        
+        @Test
+	public void MahineApp_runMachine_True() {   
+            Boolean expected = true;
+            Boolean actual;
+            actual = turing.run(prog, false);
+            assertEquals(expected, actual); 
+        }
+        
+        @Test
+	public void MahineApp_runMachine_False() { 
+            Boolean expected = false;
+            Boolean actual;
+            prog="d#010000110101";
+            actual = turing.run(prog, false);
+            assertEquals(expected, actual); 
 	}
 
 }
